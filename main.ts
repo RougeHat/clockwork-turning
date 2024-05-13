a7948848b99825574ee698d09aa1b81b3d918cb9
Tomoe0905 <109864827+Tomoe0905@users.noreply.github.com>
2024-05-13 14:57:51 +0200

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+let mySprite = sprites.create(assets.image`Bot`, SpriteKind.Player)
+mySprite.setPosition(35, 60)
+tiles.setCurrentTilemap(tilemap`level 1`)
+scene.setBackgroundColor(1)
+forever(function () {
+    scene.cameraFollowSprite(mySprite)
+    if (controller.left.isPressed() || (controller.right.isPressed() || controller.down.isPressed())) {
+        controller.moveSprite(mySprite, 100, 100)
+    }
+})
+forever(function () {
+    mySprite.setVelocity(0, 50)
+})
